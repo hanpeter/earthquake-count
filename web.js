7cd9f5bd0c220bbe0814f7cb3bf547eac5d47e83
Peter Han <phan@clicktime.com>
2015-08-18 14:31:16 -0700

    Refactoring earthquakeCounter class

--- web.js
+++ web.js
@@ -19,29 +19,47 @@ app.get('/data/sf/', function (req, res) {
 	res.json(sfCounter.count);
 });
 
-function earthquakeCounter(options, refreshTime) {
+function earthquakeCounter(options) {
 	var me = {
 		count: {},
 		load: function () {
-			var now = new Date();
-			var startOfMonth = now.getFullYear() + '-' + bufferString(now.getMonth() + 1, 2) + '-01';
-			var tomorrow = now.getFullYear() + '-' + bufferString(now.getMonth() + 1, 2) + '-' + bufferString(now.getDate() + 1, 2);
 			var resOptions = {
 				url: 'http://earthquake.usgs.gov/fdsnws/event/1/query',
-				qs: _.extend(options, {
+				qs: _.extend({
 					format: 'geojson',
-					starttime: startOfMonth,
-					endtime: tomorrow
-				}),
+					starttime: me.startTime,
+					endtime: me.endTime
+				}, options),
 				json: true
 			};
 
 			request.get(resOptions, function (error, response, body) {
 				me.count = handleEarthquakes(body);
+				me.afterLoad();
 				console.log(me.count);
 			});
-		}
+		},
+		afterLoad: options.afterLoad || function () {}
 	};
+
+	Object.defineProperties(me, {
+		startTime: {
+			enumerate: true,
+			get: function () {
+				var now = new Date();
+				return now.getFullYear() + '-' + bufferString(now.getMonth() + 1, 2) + '-01';
+			}
+		},
+		endTime: {
+			enumerate: true,
+			get: function () {
+				var now = new Date();
+				return now.getFullYear() + '-' + bufferString(now.getMonth() + 1, 2) + '-' + bufferString(now.getDate() + 1, 2);
+			}
+		}
+	});
+
+	var refreshTime = options.refreshTime || REFRESH_RATE;
 
 	function bufferString(string, length) {
 		string = string.toString();
@@ -82,11 +100,18 @@ function earthquakeCounter(options, refreshTime) {
 	return me;
 }
 
-var worldCounter = earthquakeCounter({}, REFRESH_RATE);
+var worldCounter = earthquakeCounter({
+	afterLoad: function () {
+
+	}
+});
 
 var sfOptions = {
 	latitude: 37.7833,
 	longitude: -122.4167,
-	maxradiuskm: 100
+	maxradiuskm: 100,
+	afterLoad: function () {
+
+	}
 };
-var sfCounter = earthquakeCounter(sfOptions, REFRESH_RATE);
+var sfCounter = earthquakeCounter(sfOptions);
